64752e8229b8b3a8f7e5a6aa7fa5893478aaa3f5
tw1ttt3r <tw1ttt3r@gmail.com>
2022-07-11 04:37:04 -0500

    chore: implemented Tooltip

--- src/components/RegistrationButton.tsx
+++ src/components/RegistrationButton.tsx
@@ -1,3 +1,4 @@
+import { Tooltip } from "@mui/material";
 import RegistrationIcon from "icons/RegistrationIcon";
 import { useModalStatus } from "lib/Modal.context";
 
@@ -6,11 +7,11 @@ const RegistrationButton = () => {
   const { setStatusModal } = useModalStatus();
 
   return (
-    <>
+    <Tooltip title="Agregar Tarea" placement="left">
       <span onClick={() => setStatusModal(true)} className="group hover:bg-white fixed bottom-10 right-10 bg-gray-500 rounded cursor-pointer">
         <RegistrationIcon className="text-white w-12 group-hover:text-[green]" />
       </span>
-    </>
+    </Tooltip>
   )
 }
 
