82c9b03f176f786e43b1b1923483c6a7125fc715
tw1ttt3r <tw1ttt3r@gmail.com>
2022-07-11 00:40:58 -0500

    chore: deleted unnecessary code

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -29,79 +29,11 @@ const status: Status = {
   },
 }
 
-const tasks = [
-  {
-    id: 1,
-    title: "una",
-    description: "something una",
-    status: "todo"
-  },
-  {
-    id: 2,
-    title: "dos",
-    description: "something dos",
-    status: "inprogress"
-  },
-  {
-    id: 3,
-    title: "tres",
-    description: "something tres",
-    status: "done"
-  },
-  {
-    id: 4,
-    title: "cuatro",
-    description: "something cuatro",
-    status: "todo"
-  },
-  {
-    id: 7,
-    title: "cinco",
-    description: "something cinco",
-    status: "inprogress"
-  },
-  {
-    id: 10,
-    title: "seis",
-    description: "something seis",
-    status: "inprogress"
-  },
-  {
-    id: 99,
-    title: "siete",
-    description: "something siete",
-    status: "todo"
-  },
-  {
-    id: 80,
-    title: "ocho",
-    description: "something ocho",
-    status: "done"
-  },
-  {
-    id: 34,
-    title: "nueve",
-    description: "something nueve",
-    status: "inprogress"
-  },
-  {
-    id: 99,
-    title: "diez",
-    description: "something diez",
-    status: "done"
-  },
-]
-
 const Board: FC<BoardProps> = ({ className }: BoardProps) => {
 
   const {
-    setAllTasks,
     groupedTasks
   } = useTasks();
-  
-  useEffect(() => {
-    setAllTasks(tasks)
-  }, [])
 
   return (
     <section className="w-screen h-screen bg-black p-4 flex flex-col lg:flex-row gap-4 overflow-y-auto">
