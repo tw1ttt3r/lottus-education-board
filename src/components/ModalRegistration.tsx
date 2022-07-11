2c5b0b30aa46ef361fec423ac2631dee2e7ef4aa
tw1ttt3r <tw1ttt3r@gmail.com>
2022-07-11 02:33:39 -0500

    feat: implemented hooks status modal

--- src/components/ModalRegistration.tsx
+++ src/components/ModalRegistration.tsx
@@ -5,46 +5,38 @@ import TextArea from "components/TextArea";
 import Button from "components/Button";
 import CloseIcon from "icons/CloseIcon";
 import { useTasks } from "lib/Tasks.context";
+import { useModalStatus } from "lib/Modal.context";
 
-type ModalRegistrationProps = {
-  title?: string;
-  description?: string;
-  id?: string;
-  open: boolean;
-  onChange: (status: boolean) => void
-}
+const ModalRegistration: FC = _ => {
 
-const ModalRegistration: FC<ModalRegistrationProps> = ({ open, onChange, title, description, id }: ModalRegistrationProps) => {
-
-  const [ status, setStatus ] = useState(open)
   const [ titleTask, setTitleTask ] = useState("")
   const [ descriptionTask, setDescriptionTask ] = useState("")
+  const [ idTask, setIdTask ] = useState("")
   const [ validations, setValidations ] = useState({ title: true, description: true })
-  const [ modeEdition, setModeEdition ] = useState(false)
 
   const {
     createTask,
     editedTask
   } = useTasks();
 
+  const {
+    setStatusModal,
+    statusModal,
+    currentTask,
+    modeEdition
+  } = useModalStatus();
+
 
   useEffect(() => {
-    setTitleTask(title || "")
-    setDescriptionTask(description || "")
-    if (title && description) {
-      setModeEdition(true)
-    }
-  }, [title, description])
-
-  useEffect(() => {
-    setStatus(open)
-  }, [open])
+    setTitleTask(modeEdition && !!currentTask ? currentTask.title : "")
+    setDescriptionTask(modeEdition && !!currentTask ? currentTask.description : "")
+    setIdTask(modeEdition && !!currentTask ? currentTask.id : "")
+  }, [modeEdition])
 
   const closeModal = (e: KeyboardEvent) => {
     if (e.key === "Escape") {
       cleanModal()
-      setStatus(false)
-      onChange(false)
+      setStatusModal(false)
       return
     }
   }
@@ -72,25 +64,24 @@ const ModalRegistration: FC<ModalRegistrationProps> = ({ open, onChange, title, 
     if (!modeEdition) {
       createTask(titleTask, descriptionTask)
     } else {
-      editedTask(titleTask, descriptionTask, id)
+      editedTask(titleTask, descriptionTask, idTask)
     }
-    setStatus(false)
-    onChange(false)
+    setStatusModal(false)
     cleanModal()
   }
 
   const cleanModal = () => {
     setTitleTask("")
     setDescriptionTask("")
+    setIdTask("")
     setValidations({ title: true, description: true })
   }
 
   return (
-    <section className={cn("fixed w-screen h-screen top-0 left-0 bg-[black] items-center justify-center", { "flex": status, "hidden": !status })}>
+    <section className={cn("fixed w-screen h-screen top-0 left-0 bg-[black] items-center justify-center", { "flex": statusModal, "hidden": !statusModal })}>
       <span onClick={() => {
         cleanModal()
-        setStatus(false)
-        onChange(false)
+        setStatusModal(false)
       }} className="absolute top-4 right-10">
         <CloseIcon className="w-4 text-white cursor-pointer" />
       </span>
