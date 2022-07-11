b9e922490da6cc3771566efe5c5d24d521725130
tw1ttt3r <tw1ttt3r@gmail.com>
2022-07-10 20:29:48 -0500

    chore: growned CTA add new task

--- src/components/RegistrationButton.tsx
+++ src/components/RegistrationButton.tsx
@@ -8,8 +8,8 @@ const RegistrationButton = () => {
 
   return (
     <>
-      <span onClick={() => setOpen(!open)} className="group hover:bg-white absolute bottom-10 right-10 bg-gray-500 rounded cursor-pointer">
-        <RegistrationIcon className="text-white w-6 group-hover:text-[green]" />
+      <span onClick={() => setOpen(!open)} className="group hover:bg-white fixed bottom-10 right-10 bg-gray-500 rounded cursor-pointer">
+        <RegistrationIcon className="text-white w-12 group-hover:text-[green]" />
       </span>
       <ModalRegistration onChange={(status: boolean) => setOpen(status) } open={open} />
     </>
